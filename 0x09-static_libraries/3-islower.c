52e0f7ac585befc5b3bc1a9cf53e8b82cf4c49ac
deccydessy <106523599+deccydessy@users.noreply.github.com>
2022-07-22 21:15:56 +0300

    new

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *_islower -> check if the character is lowercase
+ *@c: a character argument
+ *Return: return 1 and 0 depending on condition
+ */
+int _islower(int c)
+{
+return (c >= 'a' && c <= 'z');
+}
