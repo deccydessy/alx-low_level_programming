2dc86d6838bd80c354ef68316027f8ee2f70846c
deccydessy <desmondparsimei5@gmail.com>
2022-08-01 15:47:19 -0700

    header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,8 @@
 
 #include "dog.h"
 
+
+
 /**
 
  * main - check the code
@@ -18,15 +20,15 @@ int main(void)
   
   struct dog my_dog;
   
-  {
-    
-    char name;
-    
-    float age;
-    
-char owner
+
   
-  }
+  my_dog.name = "Poppy";
+  
+  my_dog.age = 3.5;
+  
+  my_dog.owner = "Bob";
+  
+  printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
   
   return (0);
   
