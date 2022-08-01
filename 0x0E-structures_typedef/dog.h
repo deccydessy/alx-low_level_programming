7aaf7a1de9764c2bfd5b820553561eb7f86e0714
deccydessy <desmondparsimei5@gmail.com>
2022-08-01 15:43:42 -0700

    header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#include "dog.h"
+
+/**
+
+ * main - check the code
+
+ *
+
+ * Return: Always 0.
+
+ */
+
+int main(void)
+  
+{
+  
+  struct dog my_dog;
+  
+  {
+    
+    char name;
+    
+    float age;
+    
+char owner
+  
+  }
+  
+  return (0);
+  
+}
